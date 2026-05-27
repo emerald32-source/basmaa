6e195af37a554279a185061d9f3196daf263e27e
emerald32-source <emerald320032@gmail.com>
2026-05-27 05:32:09 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,49 @@
+const clientCustomName = "بسمى";
+const clientGender = "f";
+const clientEidiyaCode = "EID-500-VIP";
+const clientEnvelopeText = `كل عام وانت بكل الخير قلب خالتو
+ربي يوفقك  ويسعدك ويحفظك 🌹
+وكل عيد وانت محققة كل اللي تتمنيه  وتحلمي بيه 
+عيدك مبارك 🌹`;
+const clientMemories = [
+    {
+        "photo": "photo1.jpg",
+        "caption": "عيدكم مبارك"
+    },
+    {
+        "photo": "photo2.jpg",
+        "caption": "يحفظكم ربي"
+    },
+    {
+        "photo": "photo3.jpg",
+        "caption": "قلب خالتو"
+    },
+    {
+        "photo": "photo4.jpg",
+        "caption": "احلي ضحكه"
+    },
+    {
+        "photo": "photo5.jpg",
+        "caption": "احلا عيون"
+    },
+    {
+        "photo": "photo6.jpg",
+        "caption": "ربي يحفظك"
+    },
+    {
+        "photo": "photo7.jpg",
+        "caption": "عيد سعيد"
+    },
+    {
+        "photo": "photo8.jpg",
+        "caption": "َ"
+    },
+    {
+        "photo": "photo9.jpg",
+        "caption": "َ"
+    },
+    {
+        "photo": "photo10.jpg",
+        "caption": "َ"
+    }
+];
